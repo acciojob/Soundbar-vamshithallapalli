c615006c3802c69bae6ec5c32b588a22653d082c
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-11-24 07:59:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+const soundNames = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+let currentAudio = null;
+
+// Play sound when clicking a button
+document.querySelectorAll(".btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    playSound(btn.textContent);
+  });
+});
+
+function playSound(name) {
+  stopSound();
+  currentAudio = new Audio(`sounds/${name}.mp3`);
+  currentAudio.play();
+}
+
+// Stop button
+document.querySelector(".stop").addEventListener("click", stopSound);
+
+function stopSound() {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+}
